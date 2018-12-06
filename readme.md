6eeb7fbf1a83674e6aefaa01e000e95ca11f4ce5
hal <staryes@me.com>
2018-12-06 17:26:19 -0500

    motor test

---
+++ readme.md
@@ -0,0 +1,7 @@
+How to use:
+
+$ roslaunch servo_cmd servo_serial.launch
+
+$ rostopic pub /m0 std_msgs/Int16 [value]
+
+
--- src/servo_cmd/src/servo_cmd.cpp
+++ src/servo_cmd/src/servo_cmd.cpp
@@ -1,8 +1,31 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Int16.h"
+#include "std_msgs/Int16MultiArray.h"
 
 #include <sstream>
+
+int servo0 = 512;
+int servo1 = 512;
+int servo2 = 512;
+int servo3 = 512;
+
+void servo0Cb(const std_msgs::Int16 msg)
+{
+    servo0 = msg.data;
+}
+void servo1Cb(const std_msgs::Int16 msg)
+{
+    servo1 = msg.data;
+}
+void servo2Cb(const std_msgs::Int16 msg)
+{
+    servo2 = msg.data;
+}
+void servo3Cb(const std_msgs::Int16 msg)
+{
+    servo3 = msg.data;
+}
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
@@ -10,12 +33,18 @@
 int main(int argc, char **argv)
 {
 
-    ros::init(argc, argv, "servo2pub");
+    ros::init(argc, argv, "servo_pub_serial");
 
     ros::NodeHandle n;
 
-    ros::Publisher servo2_pub = n.advertise<std_msgs::Int16>("Servo2Pos", 1);
+    ros::Publisher servo_pub_serial = n.advertise<std_msgs::Int16MultiArray>("ServoPoseSerial", 1);
 
+    ros::Subscriber servo_0_sub = n.subscribe<std_msgs::Int16>("m0", 1, servo0Cb);
+    ros::Subscriber servo_1_sub = n.subscribe<std_msgs::Int16>("m1", 1, servo1Cb);
+    ros::Subscriber servo_2_sub = n.subscribe<std_msgs::Int16>("m2", 1, servo2Cb);
+    ros::Subscriber servo_3_sub = n.subscribe<std_msgs::Int16>("m3", 1, servo3Cb);
+
+    
     ros::Rate loop_rate(10);
 
     int i = 0;
@@ -24,20 +53,18 @@ int main(int argc, char **argv)
         /**
          * This is a message object. You stuff it with data, and then publish it.
          */
-        std_msgs::Int16 msg;
+        std_msgs::Int16MultiArray msg;
+
+        msg.data.resize(4);
+        
+        i = 512;
+        msg.data[0] = servo0;
+        msg.data[1] = servo1;
+        msg.data[2] = servo2;
+        msg.data[3] = servo3;
 
 
-        if (i < 1024)
-        {
-            i = i + 10;
-            msg.data = i;
-        }
-        else
-        {
-            i = 0;
-        }
-
-        ROS_INFO("%d", msg.data);
+        //ROS_INFO("%d", msg.data);
 
         /**
          * The publish() function is how you send messages. The parameter
@@ -45,7 +72,7 @@ int main(int argc, char **argv)
          * given as a template parameter to the advertise<>() call, as was done
          * in the constructor above.
          */
-        servo2_pub.publish(msg);
+        servo_pub_serial.publish(msg);
 
         ros::spinOnce();
 
