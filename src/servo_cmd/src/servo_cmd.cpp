4e3a012c85753fe1f46273b55e412427e4224fb5
hal <staryes@me.com>
2018-12-06 15:45:22 -0500

    set format

--- src/servo_cmd/src/servo_cmd.cpp
+++ src/servo_cmd/src/servo_cmd.cpp
@@ -10,50 +10,50 @@
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "servo2pub");
+    ros::init(argc, argv, "servo2pub");
 
-  ros::NodeHandle n;
+    ros::NodeHandle n;
 
-  ros::Publisher servo2_pub = n.advertise<std_msgs::Int16>("Servo2Pos", 1);
+    ros::Publisher servo2_pub = n.advertise<std_msgs::Int16>("Servo2Pos", 1);
 
-  ros::Rate loop_rate(10);
+    ros::Rate loop_rate(10);
 
-int i = 0;
-  while (ros::ok())
-  {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-	std_msgs::Int16 msg;
-
-
-if (i < 1024)
-{
-i = i + 10;
-msg.data = i;
-}
-else
-{
-i = 0;
-}
-
-    ROS_INFO("%d", msg.data);
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-    servo2_pub.publish(msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-    //++count;
-  }
+    int i = 0;
+    while (ros::ok())
+    {
+        /**
+         * This is a message object. You stuff it with data, and then publish it.
+         */
+        std_msgs::Int16 msg;
 
 
-  return 0;
+        if (i < 1024)
+        {
+            i = i + 10;
+            msg.data = i;
+        }
+        else
+        {
+            i = 0;
+        }
+
+        ROS_INFO("%d", msg.data);
+
+        /**
+         * The publish() function is how you send messages. The parameter
+         * is the message object. The type of this object must agree with the type
+         * given as a template parameter to the advertise<>() call, as was done
+         * in the constructor above.
+         */
+        servo2_pub.publish(msg);
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+        //++count;
+    }
+
+
+    return 0;
 }
 
